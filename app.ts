6b5164d62f931a21fbe13b6a1836e66eb100bd76
lavdein <82412236+lavdein@users.noreply.github.com>
2023-03-17 22:32:14 +0200

    Update fetchProject

--- app.ts
+++ app.ts
@@ -23,7 +23,12 @@ app.use(async (context, next) => {
 const fetchProject = async (url: string) => {
   try {
     console.log("Fetching URL:", url);
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+      },
+    });
     const html = await response.text();
     console.log("HTML fetched:", html.slice(0, 100));
 
